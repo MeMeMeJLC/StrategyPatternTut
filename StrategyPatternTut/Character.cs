4ddcf7333fa362cc2154498669d7a6a2fabd0dd1
Jeremy Cook <jeremy.cook.llb@live.com>
2015-10-06 19:34:01 +1300

    Strategy Pattern Tutorial completed

---
+++ StrategyPatternTut/Character.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrategyPatternTut
+{
+    class Character
+    {
+        private IWeapon weapon;
+
+        public void setWeapon(IWeapon newWeapon)
+        {
+            weapon = newWeapon;
+        }
+
+        public void Attack()
+        {
+            weapon.Use();
+        }
+    }
+}
---
+++ StrategyPatternTut/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrategyPatternTut
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWeapon weapon = null;
+            Character JeremyCook = new Character();
+
+            while (true)
+            {
+                Console.WriteLine("Choose a weapon for JeremyCook! 1 = Sword, 2 = Ax, 3 = Club");
+                String input = Console.ReadLine();
+
+                switch (input)
+                {
+                    case "1":
+                        weapon = new Sword();
+                        break;
+                    case "2":
+                        weapon = new Ax();
+                        break;
+                    case "3":
+                        weapon = new Club();
+                        break;
+                    default:
+                        Console.WriteLine("no weapon picked");
+                        break;
+                }
+
+                JeremyCook.setWeapon(weapon);
+                JeremyCook.Attack();
+            }
+        }
+    }
+}
